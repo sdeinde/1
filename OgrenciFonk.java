45470fc56790833d566036d94143db38052cc779
sdeinde <sudein06@gmail.com>
2025-12-30 06:51:01 +0300

    Add files via upload

---
+++ OgrenciFonk.java
@@ -0,0 +1,17 @@
+package hafta13;
+
+import java.util.Scanner;
+
+public class OgrenciFonk {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Kaç öğrenci: ");
+		byte ks=scan.nextByte();
+		String[] ad=new String[ks];
+		byte vn[] = new byte[ks];
+		byte fn[] = new byte[ks];
+		byte bn[] = new byte[ks];
+	}
+
+}
