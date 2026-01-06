1bba8a38a66c7d9b5bd54debf61e246fc767a1a7
sdeinde <sudein06@gmail.com>
2026-01-06 15:37:12 +0300

    Create dersnotornek.java

---
+++ dersnotornek.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("- Toplama Metodu Çağırıldı.\n- İşlem Sonucu: "+toplama());
+    }
+    // Metot Tanımlaması:
+    public static int toplama(){
+        // Değişken Tanımlaması:
+        int x=23;
+        int y=624;
+        // Geriye Değer Döndürme:
+        return x+y;
+    }
+}
