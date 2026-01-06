96b70f183cbfe51722bfab4989d47390ad8e69bf
sdeinde <sudein06@gmail.com>
2026-01-06 15:52:13 +0300

    Update dersnotornek.java

--- dersnotornek.java
+++ dersnotornek.java
@@ -21,3 +21,22 @@ public class Main {
         System.out.println("Benim adım: Sude");
     }
 }
+------------------------------------------------------
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Değişken Tanımlaması:
+        int a,b;
+        System.out.print("---------\nİlk Değeri Girin: ");
+        a=scanner.nextInt();
+        System.out.print("İkinci Değeri Girin: ");
+        b=scanner.nextInt();
+        System.out.println("---------\n"+a+" + "+b+" = "+toplama(a,b));
+    }
+    // Metot Tanımlaması:
+    public static int toplama(int x,int y){
+        return x+y;
+    }
+}
