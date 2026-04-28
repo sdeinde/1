ddc39abcc42b96b946ddf204c6ccebd8edd9da21
sdeinde <sudein06@gmail.com>
2026-04-28 10:58:05 +0300

    Add files via upload

--- Ogrenci.java
+++ Ogrenci.java
@@ -1,32 +1,24 @@
-import java.util.Scanner;
+package kisi;
 
-public class Ogrenci {
+public class Ogrenci extends Kisi{
+	private String numara, bolum;
 
-	public static void main(String[] args) {
-		Scanner giris=new Scanner(System.in);
-		System.out.println("Öğrenci sayısı giriniz : ");
-		byte os=giris.nextByte();
-		String ad[]=new String[os];
-		byte []vn=new byte[os];
-		byte []fn=new byte[os];
-		byte []bn=new byte[os];
-		Fonksiyonlar.VeriAlma(giris,ad,vn,fn,bn);
-		for(;;) {
-			byte menu=Fonksiyonlar.MenuAl(giris);
-			switch (menu) {
-			case 0:System.out.println("Program bitti");
-				System.exit(0);
-				break;
-			case 1:Fonksiyonlar.Listeleme(ad,vn,fn,bn);break;
-			
-			case 2:System.out.println("aranacak Adı giriniz : ");
-				String arananAd=giris.next();
-				Fonksiyonlar.AdArama(arananAd,ad,vn,fn,bn);break;
-			case 3:Fonksiyonlar.Ortalama(vn,fn,bn);break;
-			default:
-				break;
-			}
-		}
+	public Ogrenci(String ad, String soyad, int yas, String numara, String bolum) {
+		super(ad, soyad, yas);
+		this.numara = numara;
+		this.bolum = bolum;
 	}
 
+	public String getNumara() {
+		return numara;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString()+"\nnumara : " + numara + "\nbolum :" + bolum;
+	}
+	public void bilgiGoster() {
+		super.bilgiGoster();
+		System.out.println(" Bölümü : "+bolum);
+	}
 }
